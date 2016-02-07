9968761b5da8e6148600218c1bff33492581587a
Martin Ejdestig <marejde@gmail.com>
2016-02-07 21:08:54 +0100

    Stage DecomposedMatrix4x4

---
+++ src/lib/math/decomposed_matrix4x4.h
@@ -0,0 +1,65 @@
+/**
+ * This file is part of Rayni.
+ *
+ * Copyright (C) 2013-2016 Martin Ejdestig <marejde@gmail.com>
+ *
+ * Rayni is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rayni is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Rayni. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _RAYNI_LIB_MATH_DECOMPOSED_MATRIX4x4_H_
+#define _RAYNI_LIB_MATH_DECOMPOSED_MATRIX4x4_H_
+
+#include "lib/math/math.h"
+#include "lib/math/matrix4x4.h"
+#include "lib/math/quaternion.h"
+#include "lib/math/vector3.h"
+
+namespace Rayni
+{
+	class DecomposedMatrix4x4
+	{
+	public:
+		DecomposedMatrix4x4() = default;
+
+		explicit DecomposedMatrix4x4(const Matrix4x4 &matrix);
+
+		Matrix4x4 compose() const
+		{
+			return Matrix4x4::translate(translation) * Matrix4x4::rotate(rotation) *
+			       Matrix4x4::from_axes(scale_x, scale_y, scale_z);
+		}
+
+		DecomposedMatrix4x4 interpolate(real_t t, const DecomposedMatrix4x4 &to) const
+		{
+			DecomposedMatrix4x4 d;
+
+			d.rotation = slerp(t, rotation, to.rotation);
+			d.scale_x = lerp(t, scale_x, to.scale_x);
+			d.scale_y = lerp(t, scale_y, to.scale_y);
+			d.scale_z = lerp(t, scale_z, to.scale_z);
+			d.translation = lerp(t, translation, to.translation);
+
+			return d;
+		}
+
+	private:
+		Quaternion rotation;
+		Vector3 scale_x;
+		Vector3 scale_y;
+		Vector3 scale_z;
+		Vector3 translation;
+	};
+}
+
+#endif // _RAYNI_LIB_MATH_DECOMPOSED_MATRIX4x4_H_
---
+++ src/unit_tests/lib/math/decomposed_matrix4x4.cpp
@@ -0,0 +1,63 @@
+/**
+ * This file is part of Rayni.
+ *
+ * Copyright (C) 2016 Martin Ejdestig <marejde@gmail.com>
+ *
+ * Rayni is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rayni is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Rayni. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <gtest/gtest.h>
+
+#include <cmath>
+
+#include "lib/math/matrix4x4.h"
+#include "lib/math/decomposed_matrix4x4.h"
+
+namespace Rayni
+{
+	TEST(DecomposedMatrix4x4Test, Interpolate)
+	{
+		Matrix4x4 start({2, 0, 0, 2},
+		                {0, std::cos(1) * 2, -std::sin(1) * 2, 3},
+		                {0, std::sin(1) * 2, std::cos(1) * 2, 4},
+		                {0, 0, 0, 1});
+
+		Matrix4x4 end({4, 0, 0, 4},
+		              {0, std::cos(3) * 4, -std::sin(3) * 4, 5},
+		              {0, std::sin(3) * 4, std::cos(3) * 4, 6},
+		              {0, 0, 0, 1});
+
+		Matrix4x4 middle = DecomposedMatrix4x4(start).interpolate(0.5, DecomposedMatrix4x4(end)).compose();
+
+		EXPECT_NEAR(3, middle(0, 0), 1e-6);
+		EXPECT_NEAR(0, middle(0, 1), 1e-6);
+		EXPECT_NEAR(0, middle(0, 2), 1e-6);
+		EXPECT_NEAR(3, middle(0, 3), 1e-6);
+
+		EXPECT_NEAR(0, middle(1, 0), 1e-6);
+		EXPECT_NEAR(std::cos(2) * 3, middle(1, 1), 1e-6);
+		EXPECT_NEAR(-std::sin(2) * 3, middle(1, 2), 1e-6);
+		EXPECT_NEAR(4, middle(1, 3), 1e-6);
+
+		EXPECT_NEAR(0, middle(2, 0), 1e-6);
+		EXPECT_NEAR(std::sin(2) * 3, middle(2, 1), 1e-6);
+		EXPECT_NEAR(std::cos(2) * 3, middle(2, 2), 1e-6);
+		EXPECT_NEAR(5, middle(2, 3), 1e-6);
+
+		EXPECT_NEAR(0, middle(3, 0), 1e-6);
+		EXPECT_NEAR(0, middle(3, 1), 1e-6);
+		EXPECT_NEAR(0, middle(3, 2), 1e-6);
+		EXPECT_NEAR(1, middle(3, 3), 1e-6);
+	}
+}

b60a0d1b8eeeb449e00a137f78dd3705533c2c1b
Martin Ejdestig <marejde@gmail.com>
2016-03-12 11:52:36 +0100

    Silence pylint warnings about missing documentation
    Not interested in adding extensive documentation to any Python scripts in
    tools/. Documentation will be added where it makes sense.

--- tools/printasm.py
+++ tools/printasm.py
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Rayni. If not, see <http://www.gnu.org/licenses/>.
 
+# pylint: disable=missing-docstring
+
 import json
 import os
 import re

9c28d634edef1bb6d64467787cbdffefbb18fde1
Martin Ejdestig <marejde@gmail.com>
2016-07-01 22:08:51 +0200

    Remove perf and perstat build targets
    Not that much to be gained compared to using the shell scripts directly.

--- tools/perf.sh
+++ tools/perf.sh
@@ -5,7 +5,5 @@ if [ $# -lt 1 ]; then
 	exit 1
 fi
 
-PERF_DATA=${MESON_BUILD_ROOT:-.}/perf.data
-
-perf record -o $PERF_DATA --call-graph dwarf $@
-perf report -i $PERF_DATA
+perf record --call-graph dwarf $@
+perf report

164e2a0701c84736b99dc3be92cc3e3d57d1eae5
Martin Ejdestig <marejde@gmail.com>
2016-09-06 16:10:47 +0200

    Test that C locale string_to_float/double() with empty string fails

--- src/unit_tests/lib/string/string.cpp
+++ src/unit_tests/lib/string/string.cpp
@@ -104,6 +104,7 @@ namespace Rayni
 		EXPECT_FLOAT_EQ(0.0123f, string_to_float("123e-4").value());
 		EXPECT_FLOAT_EQ(1230000.0f, string_to_float("123e+4").value());
 
+		EXPECT_FALSE(string_to_float(""));
 		EXPECT_FALSE(string_to_float("1,0"));
 		EXPECT_FALSE(string_to_float(",1"));
 		EXPECT_FALSE(string_to_float("1,"));
@@ -134,6 +135,7 @@ namespace Rayni
 		EXPECT_DOUBLE_EQ(0.0123, string_to_double("123e-4").value());
 		EXPECT_DOUBLE_EQ(1230000.0, string_to_double("123e+4").value());
 
+		EXPECT_FALSE(string_to_double(""));
 		EXPECT_FALSE(string_to_double("1,0"));
 		EXPECT_FALSE(string_to_double(",1"));
 		EXPECT_FALSE(string_to_double("1,"));

04ffb607fd9a396491d84e31201ff2e0099db075
Martin Ejdestig <marejde@gmail.com>
2016-09-09 14:49:48 +0200

    Minor cleanups in command_posix.cpp

--- src/lib/system/command_posix.cpp
+++ src/lib/system/command_posix.cpp
@@ -29,7 +29,7 @@
 
 namespace
 {
-	constexpr int CHILD_SETUP_EXIT_FAILURE = 127;
+	constexpr int CHILD_SETUP_FAILURE_EXIT_CODE = 127;
 
 	class Pipe
 	{
@@ -104,20 +104,27 @@ namespace
 		std::array<int, 2> fds = {-1, -1};
 	};
 
-	[[noreturn]] void child_exit_failure()
+	[[noreturn]] void child_setup_failure_exit()
 	{
-		std::_Exit(CHILD_SETUP_EXIT_FAILURE);
+		std::_Exit(CHILD_SETUP_FAILURE_EXIT_CODE);
 	}
 
-	[[noreturn]] void child_exec(const std::vector<std::string> &args)
+	[[noreturn]] void child_exec(const std::vector<std::string> &args, Pipe &stdout_pipe, Pipe &stderr_pipe)
 	{
+		if (!stdout_pipe.duplicate_write_fd_to(STDOUT_FILENO) ||
+		    !stderr_pipe.duplicate_write_fd_to(STDERR_FILENO))
+			child_setup_failure_exit();
+
+		stdout_pipe.close_fds();
+		stderr_pipe.close_fds();
+
 		std::vector<char *> argv(args.size() + 1, nullptr);
 		std::transform(args.begin(), args.end(), argv.begin(), [](auto &s) {
 			return const_cast<char *>(s.data());
 		});
 
 		execvp(argv[0], &argv[0]);
-		child_exit_failure();
+		child_setup_failure_exit();
 	}
 }
 
@@ -125,13 +132,30 @@ namespace Rayni
 {
 	static bool read_pipes(Pipe &stdout_pipe, Pipe &stderr_pipe, Command::Result &result)
 	{
-		std::array<char, 1024> buffer;
-		std::array<pollfd, 2> poll_fds;
+		static constexpr short int PIPE_READ_EVENTS = POLLIN | POLLHUP;
 
+		auto read_pipe_if_event_occurred = [](pollfd &poll_fd, Pipe &pipe, std::string &dest_str) {
+			if ((poll_fd.revents & PIPE_READ_EVENTS) == 0)
+				return true;
+
+			std::array<char, 1024> buffer;
+			ssize_t bytes_read = pipe.read(buffer);
+
+			if (bytes_read > 0)
+				dest_str.append(buffer.data(), static_cast<std::string::size_type>(bytes_read));
+			else if (bytes_read == 0)
+				poll_fd.fd = -1;
+			else
+				return false;
+
+			return true;
+		};
+
+		std::array<pollfd, 2> poll_fds;
 		poll_fds[0].fd = stdout_pipe.read_fd();
-		poll_fds[0].events = POLLIN | POLLHUP;
+		poll_fds[0].events = PIPE_READ_EVENTS;
 		poll_fds[1].fd = stderr_pipe.read_fd();
-		poll_fds[1].events = POLLIN | POLLHUP;
+		poll_fds[1].events = PIPE_READ_EVENTS;
 
 		while (poll_fds[0].fd >= 0 || poll_fds[1].fd >= 0)
 		{
@@ -139,31 +163,9 @@ namespace Rayni
 				if (errno != EINTR)
 					return false;
 
-			if (poll_fds[0].revents & (POLLIN | POLLHUP))
-			{
-				ssize_t bytes_read = stdout_pipe.read(buffer);
-
-				if (bytes_read > 0)
-					result.stdout.append(buffer.data(),
-					                     static_cast<std::string::size_type>(bytes_read));
-				else if (bytes_read == 0)
-					poll_fds[0].fd = -1;
-				else
-					return false;
-			}
-
-			if (poll_fds[1].revents & (POLLIN | POLLHUP))
-			{
-				ssize_t bytes_read = stderr_pipe.read(buffer);
-
-				if (bytes_read > 0)
-					result.stderr.append(buffer.data(),
-					                     static_cast<std::string::size_type>(bytes_read));
-				else if (bytes_read == 0)
-					poll_fds[1].fd = -1;
-				else
-					return false;
-			}
+			if (!read_pipe_if_event_occurred(poll_fds[0], stdout_pipe, result.stdout) ||
+			    !read_pipe_if_event_occurred(poll_fds[1], stderr_pipe, result.stderr))
+				return false;
 		}
 
 		return true;
@@ -180,27 +182,18 @@ namespace Rayni
 			return std::experimental::nullopt;
 
 		if (pid == 0)
-		{
-			if (!stdout_pipe.duplicate_write_fd_to(STDOUT_FILENO) ||
-			    !stderr_pipe.duplicate_write_fd_to(STDERR_FILENO))
-				child_exit_failure();
-
-			stdout_pipe.close_fds();
-			stderr_pipe.close_fds();
-
-			child_exec(args);
-		}
+			child_exec(args, stdout_pipe, stderr_pipe);
 
 		stdout_pipe.close_write_fd();
 		stderr_pipe.close_write_fd();
 
 		Result result;
 		bool read_success = read_pipes(stdout_pipe, stderr_pipe, result);
-		int status;
 
 		stdout_pipe.close_read_fd();
 		stderr_pipe.close_read_fd();
 
+		int status;
 		while (waitpid(pid, &status, 0) == -1)
 			if (errno != EINTR)
 				return std::experimental::nullopt;
@@ -210,7 +203,7 @@ namespace Rayni
 
 		result.exit_code = WEXITSTATUS(status);
 
-		if (result.exit_code == CHILD_SETUP_EXIT_FAILURE || !read_success)
+		if (result.exit_code == CHILD_SETUP_FAILURE_EXIT_CODE || !read_success)
 			return std::experimental::nullopt;
 
 		return std::experimental::make_optional(result);

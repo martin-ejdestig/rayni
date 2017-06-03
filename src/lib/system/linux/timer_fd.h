76fb39d95438d0811622cb9650f70f8d80a0f2f5
Martin Ejdestig <marejde@gmail.com>
2017-06-03 13:19:28 +0200

    Add TimerFD

---
+++ src/lib/system/linux/timer_fd.h
@@ -0,0 +1,190 @@
+/**
+ * This file is part of Rayni.
+ *
+ * Copyright (C) 2014-2017 Martin Ejdestig <marejde@gmail.com>
+ *
+ * Rayni is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rayni is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Rayni. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef RAYNI_LIB_SYSTEM_LINUX_TIMER_FD_H
+#define RAYNI_LIB_SYSTEM_LINUX_TIMER_FD_H
+
+#include <sys/timerfd.h>
+#include <unistd.h>
+
+#include <cerrno>
+#include <chrono>
+#include <ctime>
+#include <system_error>
+#include <utility>
+
+namespace Rayni
+{
+	// Only allows for CLOCK_MONOTONIC at the moment and assumes it is what std::chrono::steady_clock uses.
+	class TimerFD
+	{
+	public:
+		using clock = std::chrono::steady_clock;
+
+		struct Value
+		{
+			std::chrono::nanoseconds initial_expiration;
+			std::chrono::nanoseconds interval;
+		};
+
+		static constexpr clockid_t CLOCK_ID = CLOCK_MONOTONIC;
+
+		TimerFD() : timer_fd(timerfd_create(CLOCK_ID, TFD_CLOEXEC))
+		{
+			if (timer_fd == -1)
+				throw std::system_error(errno, std::system_category(), "timerfd_create() failed");
+		}
+
+		TimerFD(const TimerFD &other) = delete;
+
+		TimerFD(TimerFD &&other) noexcept : timer_fd(std::exchange(other.timer_fd, -1))
+		{
+		}
+
+		~TimerFD()
+		{
+			close();
+		}
+
+		TimerFD &operator=(const TimerFD &other) = delete;
+
+		TimerFD &operator=(TimerFD &&other) noexcept
+		{
+			close();
+			timer_fd = std::exchange(other.timer_fd, -1);
+			return *this;
+		}
+
+		int fd() const
+		{
+			return timer_fd;
+		}
+
+		void set(std::chrono::nanoseconds expiration) const
+		{
+			struct itimerspec timer_spec = {};
+
+			timer_spec.it_value = time_spec_from_ns(expiration);
+
+			settime(0, timer_spec);
+		}
+
+		void set(std::chrono::nanoseconds initial_expiration, std::chrono::nanoseconds interval) const
+		{
+			struct itimerspec timer_spec = {};
+
+			timer_spec.it_value = time_spec_from_ns(initial_expiration);
+			timer_spec.it_interval = time_spec_from_ns(interval);
+
+			settime(0, timer_spec);
+		}
+
+		void set(clock::time_point expiration) const
+		{
+			struct itimerspec timer_spec = {};
+
+			timer_spec.it_value = time_spec_from_time_point(expiration);
+
+			settime(TFD_TIMER_ABSTIME, timer_spec);
+		}
+
+		void set(clock::time_point initial_expiration, std::chrono::nanoseconds interval) const
+		{
+			struct itimerspec timer_spec = {};
+
+			timer_spec.it_value = time_spec_from_time_point(initial_expiration);
+			timer_spec.it_interval = time_spec_from_ns(interval);
+
+			settime(TFD_TIMER_ABSTIME, timer_spec);
+		}
+
+		Value get() const
+		{
+			struct itimerspec timer_spec = {};
+
+			if (timerfd_gettime(timer_fd, &timer_spec) == -1)
+				throw std::system_error(errno, std::system_category(), "timerfd_gettime() failed");
+
+			return {time_spec_to_ns(timer_spec.it_value), time_spec_to_ns(timer_spec.it_interval)};
+		}
+
+		void disarm() const
+		{
+			struct itimerspec timer_spec = {};
+
+			settime(0, timer_spec);
+		}
+
+		std::uint64_t read() const
+		{
+			std::uint64_t value = 0;
+
+			while (::read(timer_fd, &value, sizeof value) != static_cast<ssize_t>(sizeof value))
+				if (errno != EINTR)
+					throw std::system_error(errno,
+					                        std::system_category(),
+					                        "read() from timerfd failed");
+
+			return value;
+		}
+
+	private:
+		void settime(int flags, struct itimerspec &timer_spec) const
+		{
+			if (timerfd_settime(timer_fd, flags, &timer_spec, nullptr) == -1)
+				throw std::system_error(errno, std::system_category(), "timerfd_settime() failed");
+		}
+
+		void close()
+		{
+			if (timer_fd == -1)
+				return;
+
+			::close(timer_fd);
+			timer_fd = -1;
+		}
+
+		static constexpr struct timespec time_spec_from_ns(std::chrono::nanoseconds ns)
+		{
+			struct timespec time_spec = {};
+
+			time_spec.tv_sec = std::chrono::duration_cast<std::chrono::seconds>(ns).count();
+			time_spec.tv_nsec = (ns % std::chrono::seconds(1)).count();
+
+			return time_spec;
+		}
+
+		static constexpr struct timespec time_spec_from_time_point(clock::time_point time_point)
+		{
+			auto duration = time_point.time_since_epoch();
+			auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(duration);
+
+			return time_spec_from_ns(ns);
+		}
+
+		static constexpr std::chrono::nanoseconds time_spec_to_ns(const struct timespec &time_spec)
+		{
+			return std::chrono::seconds(time_spec.tv_sec) + std::chrono::nanoseconds(time_spec.tv_nsec);
+		}
+
+		int timer_fd = -1;
+	};
+}
+
+#endif // RAYNI_LIB_SYSTEM_LINUX_TIMER_FD_H
---
+++ src/unit_tests/lib/system/linux/timer_fd.cpp
@@ -0,0 +1,133 @@
+/**
+ * This file is part of Rayni.
+ *
+ * Copyright (C) 2014-2017 Martin Ejdestig <marejde@gmail.com>
+ *
+ * Rayni is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rayni is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Rayni. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <gtest/gtest.h>
+
+#include <array>
+#include <system_error>
+
+#include "lib/system/linux/epoll.h"
+#include "lib/system/linux/timer_fd.h"
+
+// TODO: Remove NOLINT when https://llvm.org/bugs/show_bug.cgi?id=25594 is fixed.
+using namespace std::chrono_literals; // NOLINT
+
+namespace Rayni
+{
+	TEST(TimerFD, FD)
+	{
+		TimerFD timer_fd;
+
+		EXPECT_NE(-1, timer_fd.fd());
+	}
+
+	TEST(TimerFD, MoveConstructor)
+	{
+		TimerFD timer_fd1;
+		int fd = timer_fd1.fd();
+
+		TimerFD timer_fd2(std::move(timer_fd1));
+
+		EXPECT_EQ(-1, timer_fd1.fd()); // NOLINT: misc-use-after-move (tests move)
+		EXPECT_EQ(fd, timer_fd2.fd());
+	}
+
+	TEST(TimerFD, MoveAssignment)
+	{
+		TimerFD timer_fd1;
+		int fd = timer_fd1.fd();
+
+		TimerFD timer_fd2;
+		timer_fd2 = std::move(timer_fd1);
+
+		EXPECT_EQ(-1, timer_fd1.fd()); // NOLINT: misc-use-after-move (tests move)
+		EXPECT_EQ(fd, timer_fd2.fd());
+	}
+
+	TEST(TimerFD, SetGetTime)
+	{
+		TimerFD timer_fd;
+		TimerFD::Value value;
+
+		timer_fd.set(10s);
+		value = timer_fd.get();
+		EXPECT_GT(10s, value.initial_expiration);
+		EXPECT_EQ(0ns, value.interval);
+
+		timer_fd.set(5s, 1s);
+		value = timer_fd.get();
+		EXPECT_GT(5s, value.initial_expiration);
+		EXPECT_EQ(1s, value.interval);
+
+		timer_fd.set(TimerFD::clock::now() + 10s);
+		value = timer_fd.get();
+		EXPECT_GT(10s, value.initial_expiration);
+		EXPECT_EQ(0ns, value.interval);
+
+		timer_fd.set(TimerFD::clock::now() + 5s, 1s);
+		value = timer_fd.get();
+		EXPECT_GT(5s, value.initial_expiration);
+		EXPECT_EQ(1s, value.interval);
+	}
+
+	TEST(TimerFD, Disarm)
+	{
+		TimerFD timer_fd;
+		TimerFD::Value value;
+
+		value = timer_fd.get();
+		EXPECT_EQ(0ns, value.initial_expiration);
+		EXPECT_EQ(0ns, value.interval);
+
+		timer_fd.set(10s, 1s);
+		timer_fd.disarm();
+
+		value = timer_fd.get();
+		EXPECT_EQ(0ns, value.initial_expiration);
+		EXPECT_EQ(0ns, value.interval);
+	}
+
+	TEST(TimerFD, PollAndRead)
+	{
+		TimerFD timer_fd;
+		Epoll epoll;
+		std::array<Epoll::Event, 1> events;
+
+		epoll.add(timer_fd.fd(), Epoll::Flag::IN);
+
+		EXPECT_EQ(0, epoll.wait(events, 0ms));
+
+		timer_fd.set(1ns);
+
+		EXPECT_EQ(1, epoll.wait(events, 1ms));
+		EXPECT_EQ(1, timer_fd.read());
+		EXPECT_EQ(0, epoll.wait(events, 0ms));
+	}
+
+	TEST(TimerFD, UseAfterMoveThrows)
+	{
+		TimerFD timer_fd1;
+		TimerFD timer_fd2(std::move(timer_fd1));
+
+		EXPECT_THROW(timer_fd1.set(10s), std::system_error); // NOLINT: misc-use-after-move (tests move)
+		EXPECT_THROW(timer_fd1.get(), std::system_error);
+		EXPECT_THROW(timer_fd1.disarm(), std::system_error);
+		EXPECT_THROW(timer_fd1.read(), std::system_error);
+	}
+}

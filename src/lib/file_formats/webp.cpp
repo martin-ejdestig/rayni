172d3e416be838d15c70edd739ba361e63ba5b8a
Martin Ejdestig <marejde@gmail.com>
2021-01-24 14:50:44 +0100

    Use MemoryMappedFile instead of stream in webp_read_file()
    Simpler (and more efficient, main reason is simplicity though).

--- src/lib/file_formats/webp.cpp
+++ src/lib/file_formats/webp.cpp
@@ -22,35 +22,28 @@
 #include <webp/decode.h>
 
 #include <cstdint>
-#include <fstream>
-#include <ios>
-#include <iterator>
-#include <string>
-#include <vector>
 
 #include "lib/function/result.h"
 #include "lib/graphics/image.h"
+#include "lib/system/memory_mapped_file.h"
 
 namespace Rayni
 {
 	Result<Image> webp_read_file(const std::string &file_name)
 	{
-		std::ifstream file(file_name, std::ios::binary);
-		if (!file.is_open())
-			return Error(file_name + ": failed to open WebP image");
-
-		std::vector<std::uint8_t> file_data((std::istreambuf_iterator<char>(file)),
-		                                    std::istreambuf_iterator<char>());
+		MemoryMappedFile file;
+		if (auto r = file.map(file_name); !r)
+			return r.error();
 
 		int width;
 		int height;
-		if (!WebPGetInfo(file_data.data(), file_data.size(), &width, &height))
+		if (!WebPGetInfo(static_cast<const std::uint8_t *>(file.data()), file.size(), &width, &height))
 			return Error(file_name + ": failed to determine size of WebP image");
 
 		Image image(static_cast<unsigned int>(width), static_cast<unsigned int>(height));
 
-		if (!WebPDecodeRGBInto(file_data.data(),
-		                       file_data.size(),
+		if (!WebPDecodeRGBInto(static_cast<const std::uint8_t *>(file.data()),
+		                       file.size(),
 		                       image.buffer().data(),
 		                       image.buffer().size(),
 		                       static_cast<int>(image.stride())))
